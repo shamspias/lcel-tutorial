55de51c64b4d84bf0cd17ca843089c790f1b8db3
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2024-03-04 20:53:45 +0600

    add groq

--- exercises/basic_examples/lecture-3-fallback with multi llm.py
+++ exercises/basic_examples/lecture-3-fallback with multi llm.py
@@ -1,6 +1,8 @@
 from langchain_openai import OpenAI, ChatOpenAI
 from langchain_community.chat_models import ChatFireworks
 from langchain_community.chat_models import GigaChat
+from langchain_groq.chat_models import ChatGroq
+from langchain_mistralai.chat_models import ChatMistralAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough, ConfigurableField
@@ -9,18 +11,20 @@ prompt = ChatPromptTemplate.from_template(
     "Tell me a short joke about {topic}"
 )
 chat_openai = ChatOpenAI(model="gpt-3.5-turbo")
-openai = OpenAI(model="gpt-3.5-turbo-instruct")
-mistral = ChatFireworks(model="mistral-small")
+firework_models = ChatFireworks(model="accounts/fireworks/models/mixtral-8x7b-instruct")
+mistral = ChatMistralAI(model="mistral-small")
+groq_models = ChatGroq(model_name="mixtral-8x7b-32768")
 gigachat = GigaChat(verify_ssl_certs=False)
 model = (
-    chat_openai
-    .with_fallbacks([mistral, gigachat])
+    groq_models
+    .with_fallbacks([mistral, firework_models, chat_openai, gigachat])
     .configurable_alternatives(
         ConfigurableField(id="model"),
-        default_key="chat_openai",
-        openai=openai,
+        default_key="groq_models",
         gigachat=gigachat,
+        chat_openai=chat_openai,
         mistral=mistral,
+        firework_models=firework_models
     )
 )
 
